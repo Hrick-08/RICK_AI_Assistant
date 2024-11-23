7a71e4589e868b7a5a0bed6ca58f1e476a014e52
Hrick-08 <hritabratadas8@gmail.com>
2024-11-23 18:22:29 +0530

    adding song play and greeting

--- main.py
+++ main.py
@@ -1,34 +1,52 @@
 import pyttsx3
 import speech_recognition as sr
+import random
+import webbrowser
 
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')       #getting details of current voice
-# for voice in voices:
 engine.setProperty('voice', voices[0].id)   
 engine.setProperty("rate",170)
 
 
-def speak(command):
-    engine.say(command)
+def speak(audio):
+    engine.say(audio)
     engine.runAndWait()
     
 
-# obtain audio from the microphone
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Say something!")
-    audio = r.listen(source)
+def command():
+    # obtain audio from the microphone
+    content = " "
+    while content == " ":
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            print("Say something!")
+            audio = r.listen(source)
 
 
-# recognize speech using Google Speech Recognition
-try:
-    # for testing purposes, we're just using the default API key
-    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-    # instead of `r.recognize_google(audio)`
-    print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
-except sr.UnknownValueError:
-    print("Google Speech Recognition could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+        # recognize speech using Google Speech Recognition
+        try:
+            content = r.recognize_google(audio, language="en-in")
+            print("You said............." + content)
 
-# speak("Hey")
+        except Exception as e:
+            print("Please try again...")
+
+    return content
+
+def main_process():
+    while True:
+        request = command().lower()
+        print(request)
+        if "hello" in request:
+            speak("Welcome. How can I assist you?")
+        elif "play music" in request:
+            speak("Playing music")
+            song = random.randint(1,3)
+            if song == 1:
+                webbrowser.open("https://open.spotify.com/track/0Fk2JzsQIhZvfm5AC0FRKG?si=da71da1110014d59")
+            elif song==2:
+                webbrowser.open("https://open.spotify.com/track/1Qrg8KqiBpW07V7PNxwwwL?si=170e0b6689b14fff")
+            elif song==3:
+                webbrowser.open("https://open.spotify.com/track/5ZF6l3xKi3m6YK2dDXAsR5?si=ad2224c3a8f84761")
+main_process()
